3fc9f1fc62a04d2a6625b587eb3446b2f819bc1f
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2015-03-30 12:50:01 -0700

    format

--- hello-test-scripts/provision.pl
+++ hello-test-scripts/provision.pl
@@ -253,103 +253,103 @@ print "
 ";
 
 while( $line = <SERIALPORT>)  {
-	print LOG $line;
-	  if( $line =~ /FreeRTOS/ ) {
-          $has200 = 0;
-          $killswitch = 0;
-          `clear`;
-          print_test_begin();
-          slow_type("\r\nboot\r\n");
-          slow_type("\r\ndisconnect\r\n");
-          slow_type("\r\n^ pause\r\n");
-          slow_type("\r\nprovision\r\n");
-	  }
-      if($killswitch == 1){
-          #noop
-          next;
-      }
-      if( $line =~ /PAIRING MODE/ ) {
-          slow_type("\r\ngenkey\r\n");
-          ualarm(0);
-          `clear`;
-          print_connecting();
+    print LOG $line;
+    if( $line =~ /FreeRTOS/ ) {
+        $has200 = 0;
+        $killswitch = 0;
+        `clear`;
+        print_test_begin();
+        slow_type("\r\nboot\r\n");
+        slow_type("\r\ndisconnect\r\n");
+        slow_type("\r\n^ pause\r\n");
+        slow_type("\r\nprovision\r\n");
+    }
+    if($killswitch == 1){
+        #noop
+        next;
+    }
+    if( $line =~ /PAIRING MODE/ ) {
+        slow_type("\r\ngenkey\r\n");
+        ualarm(0);
+        `clear`;
+        print_connecting();
 
-		  ualarm(20_000_000);
-         }
-	  if( $line =~ /SL_NETAPP_IPV4_ACQUIRED/) {
-          ualarm(0);
-          `clear`;
-          print_testing();
-          $has200 = 0;
-          slow_type("\r\ntestkey\r\n");
-          ualarm(70_000_000);
-	  }
-	  if( $line =~ /factory key: ([0-9A-Z]+)/ ) {
-          ualarm(0);
-		  my $key = $1;
-          `clear`;
-          print_scan_sense_serial();
-		  my $serial = read_serial();
-		  chomp($serial);
-		  print "Got serial ".$serial.".\r\n";
-		  
-		  my $post = "POST /v1/provision/".$serial." HTTP/1.0\r\n".
-		  "Host: provision.hello.is\r\n".
-		  "Content-type: text/plain\r\n".
-		  "Content-length: ".length($key)."\r\n".
-		  "\r\n".
-		  $key;
-		  #print $post;
+        ualarm(20_000_000);
+    }
+    if( $line =~ /SL_NETAPP_IPV4_ACQUIRED/) {
+        ualarm(0);
+        `clear`;
+        print_testing();
+        $has200 = 0;
+        slow_type("\r\ntestkey\r\n");
+        ualarm(70_000_000);
+    }
+    if( $line =~ /factory key: ([0-9A-Z]+)/ ) {
+        ualarm(0);
+        my $key = $1;
+        `clear`;
+        print_scan_sense_serial();
+        my $serial = read_serial();
+        chomp($serial);
+        print "Got serial ".$serial.".\r\n";
 
-		  my $cl = IO::Socket::SSL->new('provision.hello.is:443');
-		  print $cl $post;
+        my $post = "POST /v1/provision/".$serial." HTTP/1.0\r\n".
+        "Host: provision.hello.is\r\n".
+        "Content-type: text/plain\r\n".
+        "Content-length: ".length($key)."\r\n".
+        "\r\n".
+        $key;
+        #print $post;
 
-		  my $response = <$cl>;
-		  #print "Reply:\r\n".$response;
-		  
-		  if( $response =~ /200 OK/ ) {
-			  # Allocate MAC?
-			  slow_type("\r\nconnect hello-prov myfunnypassword 2\r\n");
-			  `clear`;
-		  } else {
-			  `clear`;
-              print_fail();
-		  }
-		  close($cl);
-	  }
-      if($line =~ /200 OK/){
-          $has200 = 1;
-      }
-      if( $line =~ /test key validated/ || ($line =~ / test key success/ && $has200 == 1)){
-          ualarm(0);
-          slow_type("\r\nloglevel 40\r\ndisconnect\r\n");
-          my $got_region = 0;
-          
-          while( !$got_region ) { #disable for demo
-          
-              print_scan_upc();
+        my $cl = IO::Socket::SSL->new('provision.hello.is:443');
+        print $cl $post;
 
-          my $upc = <>;
-          chomp($upc);
-          print "Got UPC ".$upc.".\r\n";
-          
-          # if (0) { # enable for demo
-          
-          if( exists $region_map{$upc}  ) {
-              print "Setting country code ",$region_map{$upc},"\n";
-              slow_type("\r\ncountry ",$region_map{$upc},"\r\n");
-              $got_region = 1;
-          } else {
-              print_unknown_upc();
-          }
+        my $response = <$cl>;
+        #print "Reply:\r\n".$response;
+
+        if( $response =~ /200 OK/ ) {
+            # Allocate MAC?
+            slow_type("\r\nconnect hello-prov myfunnypassword 2\r\n");
+            `clear`;
+        } else {
+            `clear`;
+            print_fail();
+        }
+        close($cl);
+    }
+    if($line =~ /200 OK/){
+        $has200 = 1;
+    }
+    if( $line =~ /test key validated/ || ($line =~ / test key success/ && $has200 == 1)){
+        ualarm(0);
+        slow_type("\r\nloglevel 40\r\ndisconnect\r\n");
+        my $got_region = 0;
+
+        while( !$got_region ) { #disable for demo
+
+            print_scan_upc();
+
+            my $upc = <>;
+            chomp($upc);
+            print "Got UPC ".$upc.".\r\n";
+
+            # if (0) { # enable for demo
+
+            if( exists $region_map{$upc}  ) {
+                print "Setting country code ",$region_map{$upc},"\n";
+                slow_type("\r\ncountry ",$region_map{$upc},"\r\n");
+                $got_region = 1;
+            } else {
+                print_unknown_upc();
+            }
         }
         print_pass();
-      }
-	  if( $line =~ /test key not valid/ ) {
-          ualarm(0);
-          `clear`;
-          print_generating_key();
-          ualarm(20_000_000);
-	  }
+    }
+    if( $line =~ /test key not valid/ ) {
+        ualarm(0);
+        `clear`;
+        print_generating_key();
+        ualarm(20_000_000);
+    }
 }
 
