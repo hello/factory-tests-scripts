a501af963f4b927909d5fa759e35a080b7bb63cb
Jackson Chen <jchen@sayhello.com>
2015-04-20 11:19:53 +0800

    sorta works

--- hello-test-scripts/provision.pl
+++ hello-test-scripts/provision.pl
@@ -33,6 +33,11 @@ open (SERIALPORT, "+<", "$port") or die "can't open $port. ";
 open (LOG, ">>", $logfile) or die "can't open $logfile. ";
 usleep(100000);
 
+sub close_and_upload{
+    my ($session, $filename) = @_;
+    close $session;
+    `./sanders pch $filename &`
+}
 sub slow_type{
     my ($str) = @_;
     if ($killswitch == 0){
@@ -176,6 +181,26 @@ sub print_test_begin{
 
 ", RESET;
 }
+sub print_timeout{
+    `clear`;
+    print RED, "
+#######   ###   #     # ####### ####### #     # #######
+#       #    ##   ## #       #     # #     #    #
+#       #    # # # # #       #     # #     #    #
+#       #    #  #  # #####   #     # #     #    #
+#       #    #     # #       #     # #     #    #
+#       #    #     # #       #     # #     #    #
+#      ###   #     # ####### #######  #####     #
+
+######  #######    #    ####### #######    #     #####  #     #
+#     # #         # #      #       #      # #   #     # #     #
+#     # #        #   #     #       #     #   #  #       #     #
+######  #####   #     #    #       #    #     # #       #######
+#   #   #       #######    #       #    ####### #       #     #
+#    #  #       #     #    #       #    #     # #     # #     #
+#     # ####### #     #    #       #    #     #  #####  #     #
+", RESET;
+}
 sub print_unknown_upc{
               print "
               
@@ -204,31 +229,6 @@ sub print_unknown_upc{
                #####  #        #####
           ";
 }
-$SIG{ALRM} = sub {
-$killswitch = 1;
-`clear`;
-print RED, "
-
-
-#######   ###   #     # ####### ####### #     # #######
-   #       #    ##   ## #       #     # #     #    #
-   #       #    # # # # #       #     # #     #    #
-   #       #    #  #  # #####   #     # #     #    #
-   #       #    #     # #       #     # #     #    #
-   #       #    #     # #       #     # #     #    #
-   #      ###   #     # ####### #######  #####     #
-
-######  #######    #    ####### #######    #     #####  #     #
-#     # #         # #      #       #      # #   #     # #     #
-#     # #        #   #     #       #     #   #  #       #     #
-######  #####   #     #    #       #    #     # #       #######
-#   #   #       #######    #       #    ####### #       #     #
-#    #  #       #     #    #       #    #     # #     # #     #
-#     # ####### #     #    #       #    #     #  #####  #     #
-
-
-", RESET;
-};
 `clear`;
 print "
 
@@ -252,74 +252,88 @@ print "
 
 
 ";
-sub verify_sn{
-    return 1;
-}
 while( 1 ) {
-    my $uut_sn;
-    print_scan_sense_serial();
-    $uut_sn = read_serial();
-    $session_logfile = "./session/$uut_sn"."_".time().".log";
-    open (SESSION, ">>", $session_logfile) or die "can't open $session_logfile. ";
-    #region
-    my $got_region = 0;
-    my $upc;
-    while( !$got_region ) { #disable for demo
-        `clear`;
-        print_scan_upc();
-        $upc = <>;
-        chomp($upc);
-        print SESSION "Got UPC ".$upc.".\r\n";
-        if( exists $region_map{$upc}  ) {
-            $got_region = 1;
-        } else {
-            print_unknown_upc();
-        }
-    }
-    while( $line = <SERIALPORT>)  {
-        print SESSION "[$time, $version] $line";
-        if( $line =~ /FreeRTOS/ ) {
+    RESTART:
+    eval{
+        print_scan_sense_serial();
+        my $uut_sn = read_serial();
+        my $session_logfile = "session/$uut_sn"."_".time().".log";
+        open (my $SESSION, ">>", $session_logfile) or die "can't open $session_logfile. ";
+        $SIG{ALRM} = sub {
+            $killswitch = 1;
+            print_timeout();
+            my $session_logfilename = $session_logfile; 
+            print $SESSION "Failed Test Timed Out\n";
+            die;
+        };
+        #region
+        my $got_region = 0;
+        my $upc;
+        while( !$got_region ) { #disable for demo
             `clear`;
-            print SESSION "Command: country code ",$region_map{$upc},"\n";
-            slow_type("\r\ncountry ",$region_map{$upc},"\r\n");
-            slow_type("\r\nboot\r\n");
-            slow_type("\r\ndisconnect\r\n");
-            slow_type("\r\n^ pause\r\n");
-        }
-        if( $line =~ "Boot completed" ){
-            `clear`;
-            slow_type("\r\ngenkey\r\n");
-            print SESSION "Command: genkey\n";
-            print_generating_key();
-        }
-        if( $line =~ /factory key: ([0-9A-Z]{256})/ ) {
-            `clear`;
-            ualarm(0);
-            my $key = $1;
-            my $post = "POST /v1/provision/".$uut_sn." HTTP/1.0\r\n".
-            "Host: provision.hello.is\r\n".
-            "Content-type: text/plain\r\n".
-            "Content-length: ".length($key)."\r\n".
-            "\r\n".
-            $key;
-            print SESSION "Post: $post\n";
-
-            my $cl = IO::Socket::SSL->new('provision.hello.is:443');
-            print $cl $post;
-
-            my $response = <$cl>;
-            print SESSION "Response: $response\n"
-
-            if( $response =~ /200 OK/ ) {
-                print SESSION "Passed Provisioning\n";
-                print_pass();
+            print_scan_upc();
+            $upc = <>;
+            chomp($upc);
+            print $SESSION "Got UPC ".$upc.".\r\n";
+            if( exists $region_map{$upc}  ) {
+                $got_region = 1;
             } else {
-                print SESSION "Failed Provisioning\n";
-                print_fail();
+                print_unknown_upc();
             }
-            close($cl);
-            close(SESSION);
-            `./sanders pch $session_logfile &`
         }
+        $killswitch = 0;
+        while( $line = <SERIALPORT>)  {
+            my $time = time();
+            print $SESSION "[$time, $version] $line";
+            print "$line";
+            if( $killswitch == 0 && $line =~ /FreeRTOS/ ) {
+                ualarm(0);
+                print $SESSION "Command: country code ",$region_map{$upc},"\n";
+                slow_type("\r\ncountry ",$region_map{$upc},"\r\n");
+                slow_type("\r\nboot\r\n");
+                slow_type("\r\ndisconnect\r\n");
+                slow_type("\r\n^ pause\r\n");
+                ualarm(5_000_000);
+            }
+            if( $killswitch == 0 && $line =~ "Boot completed" ){
+                ualarm(0);
+                $killswitch = 1;
+                slow_type("\r\ngenkey\r\n");
+                print $SESSION "Command: genkey\n";
+                print_generating_key();
+                ualarm(3_000_000);
+            }
+            if( $line =~ /factory key: ([0-9A-Z]{256})/ ) {
+                `clear`;
+                ualarm(0);
+                my $key = $1;
+                my $post = "POST /v1/provision/".$uut_sn." HTTP/1.0\r\n".
+                "Host: provision.hello.is\r\n".
+                "Content-type: text/plain\r\n".
+                "Content-length: ".length($key)."\r\n".
+                "\r\n".
+                $key;
+                print $SESSION "Post: $post\n";
+
+                my $cl = IO::Socket::SSL->new('provision.hello.is:443');
+                print $cl $post;
+
+                my $response = <$cl>;
+                print $SESSION "Response: $response\n";
+
+                if( $response =~ /200 OK/ ) {
+                    print $SESSION "Passed Provisioning\n";
+                    print_pass();
+                } else {
+                    print $SESSION "Failed Provisioning\n";
+                    print_fail();
+                }
+                close($cl);
+                close_and_upload($SESSION, $session_logfile);
+            }
+        }
+    };
+    if($@) {
+        goto RESTART;
     }
 }
