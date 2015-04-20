1a14424354f9aa752bc72f293c83b3216f1dbda9
Jackson Chen <jchen@sayhello.com>
2015-04-20 11:59:43 +0800

    delegate to cleanup script instead
    * do not call sanders directly

--- hello-test-scripts/provision.pl
+++ hello-test-scripts/provision.pl
@@ -35,7 +35,7 @@ usleep(100000);
 sub close_and_upload{
     my ($session, $filename) = @_;
     close $session;
-    `./sanders pch $filename &`
+    `./cleanup.sh $filename &`
 }
 sub slow_type{
     my ($str) = @_;
