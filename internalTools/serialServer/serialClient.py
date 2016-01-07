4b937121ae2c59ec884edc29225649a9960f1281
Brandon Clarke <brandon@sayhello.com>
2016-01-07 15:49:16 -0800

    Serial client random stuff

--- internalTools/serialServer/serialClient.py
+++ internalTools/serialServer/serialClient.py
@@ -9,15 +9,28 @@ maxSize = 4096
 #sock.setblocking(0)
 localAddr = ('192.168.128.118',43556)
 messages = []
-message = {"action":"disconnect_serial","purpose":"power_supply"}
-message = {"action":"disconnect_serial","purpose":"uut"}
-message = {"action":"disconnect_serial","purpose":"golden"}
+messages.append({"action":"disconnect_serial","purpose":"power_supply"})
+messages.append({"action":"disconnect_serial","purpose":"uut"})
+messages.append({"action":"disconnect_serial","purpose":"golden"})
+messages.append({"action":"disconnect_serial","purpose":"control_board"})
+
+
+messages.append({"action":"connect_serial","port":0,"purpose":"control_board", "baudrate":19200, "parity":'N', "stopbits":1,"bytesize":8})
+#messages.append({"action":"enable_recording","purpose":"control_board"})
+
+
 messages.append({"action":"connect_serial","port":6,"purpose":"uut", "baudrate":115200, "parity":'N', "stopbits":1,"bytesize":8})
 messages.append({"action":"enable_recording","purpose":"uut"})
+
+
 messages.append({"action":"connect_serial","port":7,"purpose":"golden", "baudrate":115200, "parity":'N', "stopbits":1,"bytesize":8})
 messages.append({"action":"enable_recording","purpose":"golden"})
+
+
 messages.append({"action":"connect_serial","port":2,"purpose":"power_supply", "baudrate":19200, "parity":'N', "stopbits":1,"bytesize":8})
 messages.append({"action":"enable_recording","purpose":"power_supply"})
+
+
 messages.append({"action":"serial_message","purpose":"power_supply","message":"SYST:REM\n"})
 messages.append({"action":"serial_message","purpose":"power_supply","message":"INST FIR\n"})
 messages.append({"action":"serial_message","purpose":"power_supply","message":"VOLT 24.0V\n"})
@@ -32,6 +45,17 @@ messages.append({"action":"serial_message","purpose":"power_supply","message":"V
 messages.append({"action":"serial_message","purpose":"power_supply","message":"CURR 2A\n"})
 messages.append({"action":"serial_message","purpose":"power_supply","message":"VOLT:PROT 5V\n"})
 messages.append({"action":"serial_message","purpose":"power_supply","message":"OUTP 1\n"})
+
+messages.append({"action":"serial_message","purpose":"control_board","message":"#RKA$\r\n"})
+messages.append({"action":"serial_message_re","purpose":"control_board","message":"#HELP$\r\n","delay":0.5,"regular_expression":"Ver\:\d\.\d"})
+messages.append({"action":"serial_message_re","purpose":"control_board","message":"#IN1$\r\n","delay":1,"regular_expression":"\d"})
+messages.append({"action":"serial_message","purpose":"control_board","message":"#RK0$\r\n"})
+messages.append({"action":"serial_message","purpose":"control_board","message":"#SK1$\r\n"})
+
+#messages.append({"action":"serial_message","message":"loglevel 40","purpose":"uut"})
+messages.append({"action":"serial_message","message":"boot","purpose":"uut"})
+messages.append({"action":"serial_status","verbose": False})
+
 #messages.append({"action":"serial_message","purpose":"power_supply","message":"OUTP 0\n"})
 #messages.append({"action":"disable_recording", "purpose":"power_supply"})
 #messages.append({"action":"disconnect_serial","purpose":"power_supply"})
@@ -48,7 +72,7 @@ for message in messages:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(localAddr)
     print message
-    sock.sendall(str(message).replace("'",'"') + '\0')
+    sock.sendall(str(message).replace("'",'"').replace("True","true").replace("False","false") + '\0')
     stuff = sock.recv(1024)
     print stuff
     sock.close()
